7dea43481807b20c22d55d219d360745fc4eaea4
Declan Newman <declan@declan-newmans-macbook-pro.local>
2011-03-10 19:05:33 +0000

    Add error controller

---
+++ fatbellyjones-web/src/main/java/com/viviquity/readmy/controllers/ErrorController.java
@@ -0,0 +1,38 @@
+package com.viviquity.readmy.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.viviquity.core.model.Event;
+import com.viviquity.db.manager.EventManager;
+
+@Controller
+public class ErrorController {
+
+	private Logger logger = Logger.getLogger(ErrorController.class);
+
+	@Autowired
+	private EventManager eventManager;
+
+	@RequestMapping(value = { "/home", "/index" })
+	public ModelAndView homeHandler(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		logger.info("Hitting home controller");
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("title", "Welcome to the Fat Belly Jones website");
+		List<Event> nextGig = eventManager.findNext(1);
+		model.put("gigs", nextGig);
+		return new ModelAndView("home", model);
+	}
+
+}
